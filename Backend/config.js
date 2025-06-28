693feb49f9f470513e563cce395a6807cf2fde92
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2025-06-28 13:30:03 +0530

    create signin endpoint

--- Backend/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    PORT_NUMBER: "3012",
-    MONGO_AUTH_URL: "mongodb+srv://ashishsinghk2020:uaqSQU6jgIcMPcBP@cluster0.2gwff.mongodb.net/second_brain?retryWrites=true&w=majority&appName=second_brain",
-    JWT_AUTH: "edusphere_online"
-};
--- Backend/src/routes/userRouter.ts
+++ Backend/src/routes/userRouter.ts
@@ -3,9 +3,13 @@ import express,{ Request, Response } from "express";
 import { User } from "../DB";
 import { Types } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"; 
+import config from "../config";
 const userRouter = express.Router();
 
 
+
+// Sign up endpoint
 userRouter.post("/signup", async (req: Request, res: Response): Promise<any> => {
   try {
     const { userName, password } = req.body;
@@ -13,6 +17,7 @@ userRouter.post("/signup", async (req: Request, res: Response): Promise<any> => 
     if (!userName || !password) {
       return res.status(400).json({
         message: "All fields are mandatory",
+        status: 400
       });
     }
 
@@ -20,6 +25,7 @@ userRouter.post("/signup", async (req: Request, res: Response): Promise<any> => 
     if (findUser) {
       return res.status(400).json({
         message: "User already exists, please sign in",
+        status: 400
       });
     }
 
@@ -46,5 +52,52 @@ userRouter.post("/signup", async (req: Request, res: Response): Promise<any> => 
   }
 });
 
+
+// Login end point
+userRouter.post("/signin", async(req: Request, res: Response): Promise<any> =>{
+  try {
+    const {userName, password} = req.body;
+    if(!userName || !password){
+      return res.status(400).json({
+        message: "All Fileds Are Mandantory",
+        status: 400
+      })
+    }
+
+    const findUser = await User.findOne({userName});
+    if(!findUser){
+      return res.status(400).json({
+        message: "User Does not exists, Please Create a new account",
+        status: 400
+      })
+    }
+    const matchPassword = await bcrypt.compare(password,findUser.password);
+    if(!matchPassword){
+      return res.status(400).json({
+        message: "Invalid Credentials",
+        status: 400
+      })
+    }
+    if(matchPassword && findUser){
+      const token = jwt.sign({user: findUser.userName},config.JWT_SCERET,{
+        expiresIn: "2h"
+      })
+      return res.status(200).json({
+        message: "Login Successfully",
+        status: 200,
+        token
+      })
+    }
+
+  } catch (error: any) {
+    console.error("Login error:", error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+ 
+
+})
 
 export { userRouter };
